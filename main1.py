eaeb2b21a7c64d1c2e5c4f7c532b5e8f7302a33d
javoxir21 <zhziedullaev@gmail.com>
2022-12-03 11:29:19 +0300

    Создал main1.py файл

---
+++ main1.py
@@ -0,0 +1,12 @@
+amal = input("Nima qilamiz? (+,-)")
+a = float(input("1-raqam kiritamiz:"))
+b = float(input("2-raqam kiriting:"))
+if amal == "+":
+    C = a + b
+    print("Natija:" + str(c))
+elif amal == "-":
+    c = a - b
+    print("Natija:" + str(c))
+else:
+    print("Notogri amal kiritdingiz!")
+    
